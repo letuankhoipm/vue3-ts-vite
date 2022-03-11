3277b1ce03805acf778e37cf6d602ceeb9173db4
letuankhoi.dev <letuankhoi.dev@gmail.com>
2022-03-11 15:37:01 +0700

    test: testing commit

--- src/modules/landing-page/views/home/home.vue
+++ src/modules/landing-page/views/home/home.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>home works!</div>
+  <div>home works!
+    <p>
+      hello world
+    </p>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
